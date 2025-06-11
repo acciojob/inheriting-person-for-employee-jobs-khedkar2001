4fa64852b63d4e45be4fc23838484adc8ecd03fc
Saurabh Khedkar <70061591+khedkar2001@users.noreply.github.com>
2025-06-11 13:37:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-// Person class definition
+// Person class
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
 Person.prototype.greet = function () {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
-// Employee class definition inheriting from Person
+// Employee class inheriting from Person
 function Employee(name, age, jobTitle) {
-  // Call the Person constructor to initialize name and age
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-// Set up inheritance
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
-// Add jobGreet method to Employee
 Employee.prototype.jobGreet = function () {
-  console.log(
-    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
-  );
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
 // Do not change code below this line
